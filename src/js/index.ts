e8d11fde8e7ac972936abd0ea2260f2a512aaaec
LeafField <109466326+LeafField@users.noreply.github.com>
2023-02-06 18:09:47 +0900

    contentページ作成開始

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,12 @@
 import "../styles/style.scss";
 
-// import "@fortawesome/fontawesome-free/js/fontawesome";
-// import "@fortawesome/fontawesome-free/js/brands";
-// import "@fortawesome/fontawesome-free/js/solid";
-
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import {} from "@fortawesome/free-regular-svg-icons";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTwitter,
+  faFacebookF,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
-library.add(faBars);
+library.add(faBars, faTwitter, faFacebookF, faInstagram);
 dom.watch();
