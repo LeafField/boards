87fa687d24f34991f3fb4160d6bbb63ed5649646
LeafField <109466326+LeafField@users.noreply.github.com>
2023-02-07 20:23:49 +0900

    完成！

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,23 @@
 import "../styles/style.scss";
 
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
 import {
   faTwitter,
   faFacebookF,
   faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
 
-library.add(faBars, faTwitter, faFacebookF, faInstagram);
+library.add(faBars, faTwitter, faFacebookF, faInstagram, faX);
 dom.watch();
+
+const hamburger = document.querySelector(".navbtn") as HTMLButtonElement;
+const html = document.querySelector("html") as HTMLElement;
+
+hamburger.addEventListener("click", () => {
+  html.classList.toggle("open");
+});
+
+window.addEventListener("resize", () => {
+  html.classList.remove("open");
+});
